1a576a938e82c0f2e5c2eedbd9dc13fac53b1d8e
coder-muyi <kasopeadeola@gmail.com>
2022-05-08 23:10:11 +0100

    Completed country card design

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,13 +1,16 @@
 import styled from 'styled-components'
 
 const CountryCard = (props) => {
-  const {name, flags} = props
-  console.log(props)
+  const {name, flags, population, region, capital} = props
+  
   return (
     <StyledCountry>
       <Flag src={flags.png} alt={name.common} />
       <CountryInfo>
-        <p class="name">{"Country Name" && name.common}</p>
+        <p className="name">{"Country Name" && name.common}</p>
+        <p className="other-info">Population: <span className="light-text">{population}</span></p>
+        <p className="other-info">Region: <span className="light-text">{region}</span></p>
+        <p className="other-info">Capital: <span className="light-text">{capital}</span></p>
       </CountryInfo>
     </StyledCountry>
   )
@@ -26,16 +29,31 @@ const StyledCountry = styled.div`
 
 const Flag = styled.img`
   width: 100%;
-  /* height: 200px; */
+  height: 200px;
 `
 
 const CountryInfo = styled.div`
   padding: 0 1em;
+  height: 200px;
 
   .name { 
     font-size: 1.5em;
-    font-weight: 600;
+    font-weight: 700;
+    margin-block: .8em;
   }
+
+  .other-info {
+    padding: 0;
+    margin: .15em 0;
+    font-weight: 600;
+    font-size: 1.1em;
+
+    .light-text {
+      font-weight: 200;
+      font-size: 1.15em;
+    }
+  }
+
 `
 
 export default CountryCard
