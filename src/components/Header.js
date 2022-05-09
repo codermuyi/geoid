c9e61ac6a9796e00dcd158984a42d97b02cb0a51
coder-muyi <kasopeadeola@gmail.com>
2022-05-09 22:38:12 +0100

    Added world.png image

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,6 +13,7 @@ const Header = () => {
 
   return (
     <StyledHeader scroll={windowScroll}>
+      <img src="world.png" alt="earth"/>
       <Link to="/">
         <Logo />
       </Link>
@@ -24,12 +25,21 @@ const StyledHeader = styled.div`
   position: fixed;
   top: 0;
   width: 100%;
-  padding: .5em 1em;
+  padding: .5em;
   box-shadow: ${props => props.scroll ? "0 3px 4px rgba(0, 0, 0, 0.1)" : "0"};
   background: white;
   display: flex;
   transition-duration: .3s;
   z-index: 100;
+
+  img {
+    width: ${props => props.scroll ? "60px" : "0"};
+    height: 50px;
+    padding-right: .5em;
+    flex-shrink: 0;
+    object-fit: cover;
+    transition-duration: inherit;
+  }
 `
 
 export default Header
