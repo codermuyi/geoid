9146185b3ff43663d0bbad6c631b7804c087b4bc
coder-muyi <kasopeadeola@gmail.com>
2022-05-12 00:53:50 +0100

    Fixed scrolling problems

--- src/components/SharedLayout.js
+++ src/components/SharedLayout.js
@@ -1,12 +1,22 @@
+import { useEffect } from "react"
 import { Outlet, useLocation } from "react-router-dom"
 import Header from './Header'
 import Footer from './Footer'
 
-const SharedLayout = () => {
-  console.log(useLocation())
+function ForceScrollToTop() {
+  const { pathname } = useLocation();
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+const SharedLayout = () => {
   return (
     <div>
+      <ForceScrollToTop />
       <Header />
       <Outlet />
       <Footer />
