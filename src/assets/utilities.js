c34affdd5daa920c73d6d91039798394bcfcbe7b
coder-muyi <kasopeadeola@gmail.com>
2022-05-22 14:03:36 +0100

    deleted displayFetchResult()

--- src/assets/utilities.js
+++ src/assets/utilities.js
@@ -1,16 +1,1 @@
-import Loader from "../components/Loader"
-import Error from "../components/Error"
-
-export function displayFetchResults(status, result) {
-  switch (status) {
-    case "error":
-      return <Error fetch />
-    case "fetching":
-      return <Loader />
-    case "fetched":
-      return result
-    default:
-      return <Loader />
-  }
-}
 
