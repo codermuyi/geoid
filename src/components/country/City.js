9b3de9b491ebaa4faa50ae2846eff354e40a6310
coder-muyi <kasopeadeola@gmail.com>
2022-07-12 14:46:40 +0100

    Disabled city button skeleton

--- src/components/country/City.js
+++ src/components/country/City.js
@@ -1,15 +1,15 @@
 import useFetch from "../../assets/hooks/useFetch"
 import Button from "../common/Button"
-import Skeleton from "../CustomSkeleton"
+// import Skeleton from "../CustomSkeleton"
 import { displayFetchResults } from "../../assets/utilities"
 
 const Cities = ({ city }) => {
   const [cityInfo, cityInfoStatus] = useFetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${city}`)
 
   return displayFetchResults({
-    "fetching": <Button noShadow pad={0} bgColor="blue">
-      <Skeleton width={100} height={35} />
-    </Button>,
+    // "fetching": <Button noShadow pad={0} bgColor="blue">
+    //   <Skeleton width={100} height={35} />
+    // </Button>,
     "fetched": <Button as="a" href={cityInfo.content_urls?.desktop.page} padY={.5} padX={1} fontSize={.8}>
       {city}
     </Button>,
